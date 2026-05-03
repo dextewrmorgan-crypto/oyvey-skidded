45b0a7df6768586da332318e757f81d42e6f7667
dextewrmorgan-crypto <dextewrmorgan@gmail.com>
2026-05-03 15:15:43 -0400

    Update Spawnerdetect.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/Spawnerdetect.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/Spawnerdetect.java
@@ -1,50 +1,14 @@
-package com.donutspawnerdetect;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.tileentity.MobSpawnerBaseLogic;
-import net.minecraft.tileentity.TileEntityMobSpawner;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.WorldEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Mod(modid = SpawnerDetect.MODID, name = SpawnerDetect.NAME, version = SpawnerDetect.VERSION)
-public class SpawnerDetect {
-    public static final String MODID = "spawnerdetect";
-    public static final String NAME = "Spawner Detect";
-    public static final String VERSION = "1.0";
-
-    private static final List<BlockPos> spawnerPositions = new ArrayList<>();
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent
-    public void onWorldLoad(WorldEvent.Load event) {
-        if (event.getWorld().isRemote) {
-            spawnerPositions.clear();
-            findSpawners(event.getWorld());
+// Iterate through loaded chunks in a specific radius
+for (Chunk chunk : loadedChunks) {
+    for (int x = 0; x < 16; x++) {
+        for (int z = 0; z < 16; z++) {
+            // Scan from current height down to bedrock
+            for (int y = playerY; y >= -64; y--) {
+                BlockPos pos = new BlockPos(chunk.getPos().getStartX() + x, y, chunk.getPos().getStartZ() + z);
+                if (world.getBlockState(pos).isOf(Blocks.SPAWNER)) {
+                    this.notifyStaff(pos); // Logic to trigger the highlight
+                }
+            }
         }
     }
-
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public void onRenderWorldLast(RenderWorldLastEvent event) {
-        Minecraft mc = Minecraft.getMinecraft();
-        for (BlockPos pos : spawnerPositions) {
-            double x = pos.getX() - mc.getRenderManager().viewerPosX;
-            double y = pos.getY() - mc.getRenderManager().viewerPosY;
-            double z = pos.getZ
+}
